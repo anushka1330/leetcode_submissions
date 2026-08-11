28f6bb7cb7bee5db47dfcc141f1234b8f8d2e6d1
anushka1330 <anushka97743@gmail.com>
2026-08-11 22:07:34 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.64%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>a;
+        for(int i=1;i<=n;i++)
+        a.push_back(i%15==0?"FizzBuzz":i%3==0?"Fizz":i%5==0?"Buzz":to_string(i));
+        return a;
+    }
+};
